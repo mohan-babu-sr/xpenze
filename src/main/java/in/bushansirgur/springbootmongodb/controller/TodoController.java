4ce8c0527eb7db2f23c6d4f0aa369940ab9b5045
MOHAN BABU S R <83107890+mohan-drive@users.noreply.github.com>
2022-06-02 22:48:23 +0530

    Add files via upload

--- src/main/java/in/bushansirgur/springbootmongodb/controller/TodoController.java
+++ src/main/java/in/bushansirgur/springbootmongodb/controller/TodoController.java
@@ -7,19 +7,14 @@ import javax.validation.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import in.bushansirgur.springbootmongodb.exception.TodoCollectionException;
 import in.bushansirgur.springbootmongodb.model.TodoDTO;
 import in.bushansirgur.springbootmongodb.service.TodoService;
 
 @RestController
+@CrossOrigin("*")
 public class TodoController {
 	
 	@Autowired
